ec6188f454955dda6c766229f6bc73ab282f1c4b
ALDOSANTOSS <aldo.santos.oliveira95@gmail.com>
2023-09-11 10:03:08 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "sec-grp"
+  description = "Allow HTTP and SSH traffic via Terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "webserver" {
+  ami                    = "ami-0eb260c4d5475b901"
+  instance_type          = "t2.micro"
+  key_name               = "iac-alura"
+  tags                   = {
+    Name = "ExampleEC2WebServer"
+  }
+}
